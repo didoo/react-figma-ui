2f8d76437e1c52dd197f5143f25221e9522e5be1
Jakub Biesiada <biesiadajakub@icloud.com>
2020-07-20 18:59:09 +0200

    chore: update stprybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,12 +4,11 @@ import { addDecorator } from '@storybook/react';
 addDecorator((storyFn) => (
   <div
     style={{
+      minWidth: 200,
       maxWidth: 300,
+      width: '100%',
       height: '100vh',
       margin: '0 auto',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
     }}
   >
     {storyFn()}
